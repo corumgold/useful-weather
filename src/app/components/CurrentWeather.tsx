8da804433df1779ae00671afe5df3b2033ad39f0
Corum Gold <goldcorum@gmail.com>
2023-09-08 22:13:21 -0400

    Add current weather condition

--- src/app/components/CurrentWeather.tsx
+++ src/app/components/CurrentWeather.tsx
@@ -6,29 +6,48 @@ import { currentWeatherAPI } from "../utils";
 const CurrentWeather: React.FC = () => {
   const [currentTemp, setCurrentTemp] = useState<number>(0);
   const [currentFeelsTemp, setCurrentFeelsTemp] = useState<number>(0);
+  const [currentCondition, setCurrentCondition] = useState<string>("");
+  const [locationName, setLocationName] = useState<string>("");
+  const [locationRegion, setLocationRegion] = useState<string>("");
   const [error, setError] = useState<String>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     async function fetchCurrentWeather() {
+      setLoading(true);
       const response = await fetch(currentWeatherAPI);
       const data = await response.json();
+      console.log(data);
 
       if (response.status === 200) {
+        setLoading(false);
         setError("");
-        setCurrentTemp(data.current.temp_f);
-        setCurrentFeelsTemp(data.current.feelslike_f);
+        setCurrentTemp(Math.floor(data.current.temp_f));
+        setCurrentFeelsTemp(Math.floor(data.current.feelslike_f));
+        setCurrentCondition(data.current.condition.text);
+        setLocationName(data.location.name);
+        setLocationRegion(data.location.region);
       } else {
         setError("There was an issue fetching the weather data.");
       }
     }
     fetchCurrentWeather();
-  });
+  }, []);
 
-  return (
-    <div className="rounded-xl border p-4">
-      <h3 className="pb-2  text-center text-xl">Temperature: {currentTemp}</h3>
-      <h3 className="text-center text-xl">Feels Like: {currentFeelsTemp}</h3>
-    </div>
+  return error ? (
+    <h3>{error}</h3>
+  ) : (
+    !loading && (
+      <div className="rounded-xl border p-4 ">
+        <p className="pb-2  text-center">
+          In {locationName}, {locationRegion}, the current weather is:
+        </p>
+        <h3 className="pb-2  text-center text-xl">
+          {currentTemp} (Feels Like: {currentFeelsTemp})
+        </h3>
+        <p className="text-center text-lg">{currentCondition}</p>
+      </div>
+    )
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { Suspense } from "react";
 import CurrentWeather from "./components/CurrentWeather";
 
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
       <h1 className="p-4 text-center text-4xl">Current Weather</h1>
-      <CurrentWeather />
+        <CurrentWeather />
     </main>
   );
 }
